1e1824c6aaac0145f36735f056b2d906c6606a63
rxxuzi <75666622+rxxuzi@users.noreply.github.com>
2023-08-22 04:03:55 +0900

    Study

--- step31~40/step35.py
+++ step31~40/step35.py
@@ -1,4 +1,18 @@
 # tanh
 import numpy as np
 from dezero import Var
-# from dezero.uti;
+import dezero.functions as F
+
+x = Var(np.array(1.0))
+y = F.tanh(x)
+x.name = 'x'
+y.name = 'y'
+y.backward(create_graph=True)
+
+iters = 2
+
+for i in range(iters):
+    gx = x.grad
+    print(gx)
+    x.cleargrad()
+    gx.backward(create_graph = True)
